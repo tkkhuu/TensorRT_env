9a4677eea203dfccbe6e1506f1954e307f18cdec
Tri Khuu <khatrikhuu@gmail.com>
2020-09-02 16:50:07 -0700

    installation from scratch

---
+++ start_container.sh
@@ -0,0 +1,17 @@
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+CONTAINER_USER=tensorrt
+
+docker run -it \
+           --rm \
+           --gpus all \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY" \
+           --user=$CONTAINER_USER \
+           --volume=`pwd`/shared_dir:/home/$CONTAINER_USER/shared_dir \
+           --volume=/home/tri/Projects/YoloV5/tensorrtx/:/home/$CONTAINER_USER/shared_dir/tensorrtx \
+	   ubuntu18_trt7 
